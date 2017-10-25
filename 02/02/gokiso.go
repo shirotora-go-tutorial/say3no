2f9b3a00f74058fa080507e5664008b692a6f56d
say3no <say3no@gmail.com>
2017-10-25 19:30:01 +0900

    0202 Go基礎

---
+++ 02/02/gokiso.go
@@ -0,0 +1,15 @@
+package _2
+
+//var variableName type
+//var vname1, vname2, vname3 type
+
+// 変数宣言、変数名、型、値の順。常に変わらない？
+//var variableName type = value
+//var vname1, vname2, vname3 type = v1,v2,v3
+
+// 直接型の宣言を無視することができるので、上のコードはこのようにも書けます：
+// var vname1, vname2, vname3 = v1, v2, v3
+
+//これでもまだ面倒ですか？ええ、私もそう思います。更に簡単にしてみましょう。
+
+vname1, vname2, vname3 := v1,v2,v3
