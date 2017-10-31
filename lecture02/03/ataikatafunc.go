a5bf50d9e532fb02da079d0da5bf1cc964ec7930
say3no <say3no@gmail.com>
2017-10-31 19:33:13 +0900

    ひとまず"値、型としての関数"の写経

---
+++ lecture02/03/ataikatafunc.go
@@ -0,0 +1,52 @@
+/*
+ Goでは関数も変数の一種です。 typeを通して定義します。<T>みたいな。
+ これは、全て同じ引数と同じ戻り値を持つ一つの型です。
+ type typeName func(input1 inputType1, input2 inputType2 [, ...]) (result1 resultType1 [, ...])
+ この辺もjsっぽいよね
+
+ 関数を方として扱うことにメリットは有るのでしょうか？ では、この型の関数を値として渡してみましょう。以下の例をご覧ください。
+*/
+
+package main
+
+import "fmt"
+
+type testInt func(int) bool // 関数の型を宣言します
+
+func isOdd(integer int) bool {
+	if integer%2 == 0 {
+		return false
+	}
+	return true
+}
+
+func isEven(integer int) bool {
+	if integer%2 == 0 {
+		return true
+	}
+	return false
+}
+
+// ここでは宣言する関数の型を引数のひとつとみなします。
+func filter(slice []int, f testInt) []int {
+	var result []int
+	for _, value := range slice {
+		if f(value) {
+			result = append(result, value)
+		}
+	}
+	return result
+}
+
+func main() {
+	slice := []int{1, 2, 3, 4, 5, 6, 7}
+	fmt.Println("slice = ", slice)
+	odd := filter(slice, isOdd) // 関数の値渡し
+	fmt.Println("Odd elements of slice are : ", odd)
+	even := filter(slice, isEven) // 関数の値渡し
+	fmt.Println("Odd elements of slice are : ", even)
+
+}
+
+
+
